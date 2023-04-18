3b5c858fb92f1c021bebb075944338dc8a00c57b
tuannhan <nhan.nvtn@tctav.com>
2023-04-18 14:44:18 +0700

    Validate Request Body

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforescast/common/Location.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforescast/common/Location.java
@@ -1,5 +1,8 @@
 package com.skyapi.weatherforescast.common;
 
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,29 +24,35 @@ public class Location {
 
     @Id
     @Column(length = 12, nullable = false, unique = true)
-    @NotBlank(message = "Location code cannot be left blank")
+    @NotNull(message = "Location code cannot be null")
+    @Length(min = 3, max = 12, message = "Location code must have 3-12 characters")
     private String code;
 
     @Column(length = 128, nullable = false)
     @JsonProperty("city_name")
-    @NotBlank(message = "City name cannot be left blank")
+    @NotNull(message = "City name cannot be null")
+    @Length(min = 3, max = 128, message = "City name must have 3-128 characters")
     private String cityName;
 
     @Column(length = 128)
     @JsonProperty("region_name")
+    @Length(min = 3, max = 128, message = "Region name must have 3-128 characters")
     private String regionName;
 
     @Column(length = 64, nullable = false)
     @JsonProperty("country_name")
-    @NotBlank(message = "Country name cannot be left blank")
+    @NotNull(message = "Country name cannot be null")
+    @Length(min = 3, max = 64, message = "City name must have 3-64 characters")
     private String countryName;
 
     @Column(length = 2, nullable = false)
     @JsonProperty("country_code")
-    @NotBlank(message = "Country code cannot be left blank")
+    @NotNull(message = "Country code cannot be null")
+    @Length(min = 2, max = 2, message = "Country code must have 2 characters")
     private String countryCode;
 
     private boolean enabled;
 
+    @JsonIgnore
     private boolean trashed;
 }

55312b35d7d7691302af78f7f1c304be7186e7ba
tuannhan <tuannhan6@gmail.com>
2023-04-19 22:42:29 +0700

    Format

--- WeatherApiService/src/main/java/com/skyapi/weatherforescast/CommonUtility.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforescast/CommonUtility.java
@@ -6,18 +6,18 @@ import org.slf4j.LoggerFactory;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class CommonUtility {
-    private static Logger LOGGER = LoggerFactory.getLogger(CommonUtility.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(CommonUtility.class);
 
-    public static String getIPAddress(HttpServletRequest request) {
-        String ip = request.getHeader("X-FORWARDED-FOR");
+	public static String getIPAddress(HttpServletRequest request) {
+		String ip = request.getHeader("X-FORWARDED-FOR");
 
-        if (ip == null || ip.isEmpty()) {
-            ip = request.getRemoteAddr();
-        }
+		if (ip == null || ip.isEmpty()) {
+			ip = request.getRemoteAddr();
+		}
 
-        LOGGER.info("Client's IP Address: " + ip);
+		LOGGER.info("Client's IP Address: " + ip);
 
-        return ip;
-    }
+		return ip;
+	}
 
 }

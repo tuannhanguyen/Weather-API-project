a573041b73f0e7baffb9a26b88d21c2ac2cc38b8
tuannhan <nhan.nvtn@tctav.com>
2023-04-25 15:33:17 +0700

    Order Json Property

--- WeatherApiService/src/main/java/com/skyapi/weatherforescast/hourly/HourlyWeatherDTO.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforescast/hourly/HourlyWeatherDTO.java
@@ -1,9 +1,14 @@
 package com.skyapi.weatherforescast.hourly;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import lombok.Data;
 
 @Data
+@JsonPropertyOrder("hour_of_day")
 public class HourlyWeatherDTO {
+    @JsonProperty("hour_of_day")
     private int hourOfDay;
     private int temperature;
     private int precipitation;
--- WeatherApiService/src/main/java/com/skyapi/weatherforescast/hourly/HourlyWeatherListDTO.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforescast/hourly/HourlyWeatherListDTO.java
@@ -3,12 +3,15 @@ package com.skyapi.weatherforescast.hourly;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class HourlyWeatherListDTO {
     private String location;
 
+    @JsonProperty("hourly_forecast")
     private List<HourlyWeatherDTO> hourlyForecast = new ArrayList<>() ;
 
     public void addWeatherHourlyDTO(HourlyWeatherDTO dto) {

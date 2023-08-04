2f878238e9ccd44ef2c94a15d2856edab525503f
tuannhan <nhan.nvtn@tctav.com>
2023-08-04 14:28:10 +0700

    remove unused

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforescast/common/RealtimeWeather.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforescast/common/RealtimeWeather.java
@@ -27,36 +27,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RealtimeWeather {
     @Id @Column(name = "location_code")
-    @JsonIgnore
     private String locationCode;
 
-    @Range(min = -50, max = 50, message = "Temperature must be in the range of -50 to 50 Celsius degree")
     private int temperature;
 
-    @Range(min = 0, max = 100, message = "Humidity must be in the range of 0 to 100 percentage")
     private int humidity;
 
-    @Range(min = 0, max = 100, message = "Precipation must be in the range of 0 to 100 percentage")
     private int precipation;
 
     @JsonProperty("wind_speed")
-    @Range(min = 0, max = 200, message = "Wind speed must be in the range of 0 to 200 km/h")
     private int windSpeed;
 
     @Column(length = 50)
     @NotBlank
-    @Length(min = 3, max = 50, message = "Status must be in between 3-50 characters")
     private String status;
 
     @JsonProperty("last_updated")
-    @JsonIgnore
     private Date lastUpdated;
 
     @OneToOne
     @JoinColumn(name = "location_code")
     @MapsId
-    @JsonIgnore
     private Location location;
-
-
 }
